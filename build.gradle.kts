d2162e10f1e8bb43269dc9ce79d11c43623b5725
Lamba <basti.lamberto@gmail.com>
2018-06-25 14:15:01 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.2.50"
+}
+
+group = "it.lamba"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("com.googlecode.aima-java", "aima-core", "3.0.0")
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/it/lamba/bayesian/net/Main.kt
@@ -0,0 +1,5 @@
+package it.lamba.bayesian.net
+
+fun main(args: Array<String>){
+
+}
