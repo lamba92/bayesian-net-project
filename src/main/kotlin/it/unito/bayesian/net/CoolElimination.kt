9e3a770d3249c74b62ea5ba1d5e5d9e0e12ed0d2
Lamba <basti.lamberto@gmail.com>
2018-06-26 09:59:52 +0200

    fixes

---
+++ src/main/kotlin/it/unito/bayesian/net/CoolElimination.kt
@@ -0,0 +1,12 @@
+package it.unito.bayesian.net
+
+import aima.core.probability.RandomVariable
+import aima.core.probability.bayes.BayesianNetwork
+import aima.core.probability.bayes.exact.EliminationAsk
+
+class CoolElimination: EliminationAsk() {
+//    override fun order(bn: BayesianNetwork, vars: MutableCollection<RandomVariable>): List<RandomVariable> {
+//        bn.getNode(vars.elementAt(0))
+//
+//    }
+}
--- src/main/kotlin/it/unito/bayesian/net/Main.kt
+++ src/main/kotlin/it/unito/bayesian/net/Main.kt
@@ -1,3 +1,4 @@
+import aima.core.probability.CategoricalDistribution
 import aima.core.probability.RandomVariable
 import aima.core.probability.bayes.BayesianNetwork
 import aima.core.probability.bayes.exact.EliminationAsk
@@ -6,35 +7,30 @@ import aima.core.probability.example.ExampleRV
 import aima.core.probability.proposition.AssignmentProposition
 import java.util.*
 
-object HeuristicElimination {
+fun main(args: Array<String>) {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
+    val bn = BayesNetExampleFactory
+            .constructBurglaryAlarmNetwork()
 
-        val bn = BayesNetExampleFactory
-                .constructBurglaryAlarmNetwork()
+    val bayesInference = object : EliminationAsk() {
 
-        val bayesInference = object : EliminationAsk() {
+        override fun order(bn: BayesianNetwork?, vars: Collection<RandomVariable>): List<RandomVariable> {
 
-            override fun order(bn: BayesianNetwork?, vars: Collection<RandomVariable>): List<RandomVariable> {
+            val order = ArrayList(vars)
+            order.sortWith(Comparator.comparingInt { o -> o.domain.size() })
 
-                val order = ArrayList(vars)
-                order.sortWith(Comparator.comparingInt { o -> o.domain.size() })
-
-                return super.order(bn, vars)
-            }
+            return super.order(bn, vars)
         }
-
-        val d1 = bayesInference.ask(
-                arrayOf<RandomVariable>(ExampleRV.MARY_CALLS_RV),
-                arrayOf(), bn)
-
-        val d2 = bayesInference.ask(
-                arrayOf<RandomVariable>(ExampleRV.MARY_CALLS_RV),
-                arrayOf(AssignmentProposition(ExampleRV.MARY_CALLS_RV, true)), bn)
-
-        println(d1.values)
-        println(d2.values)
     }
 
+//    val d1 = bayesInference.ask(
+//            arrayOf<RandomVariable>(ExampleRV.MARY_CALLS_RV),
+//            arrayOf(), bn)
+
+    val d2: CategoricalDistribution = bayesInference.ask(
+            arrayOf<RandomVariable>(ExampleRV.BURGLARY_RV),
+            arrayOf(AssignmentProposition(ExampleRV.MARY_CALLS_RV, true)), bn)
+
+//    println(d1.values)
+    println(d2.toString())
 }
