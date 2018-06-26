5b9142f184002712d6a5ae54d9ff25b83ab3c9df
Lamba <basti.lamberto@gmail.com>
2018-06-26 11:04:45 +0200

    fixes

--- src/main/kotlin/it/unito/bayesian/net/Main.kt
+++ src/main/kotlin/it/unito/bayesian/net/Main.kt
@@ -6,28 +6,22 @@ import aima.core.probability.example.BayesNetExampleFactory
 import aima.core.probability.example.ExampleRV
 import aima.core.probability.proposition.AssignmentProposition
 import java.util.*
+import java.util.Comparator.comparingInt
+import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
 
     val bn = BayesNetExampleFactory
             .constructBurglaryAlarmNetwork()
 
-    val bayesInference = object : EliminationAsk() {
-
-        override fun order(bn: BayesianNetwork?, vars: Collection<RandomVariable>): List<RandomVariable> {
-
-            val order = ArrayList(vars)
-            order.sortWith(Comparator.comparingInt { o -> o.domain.size() })
-
-            return super.order(bn, vars)
-        }
+    val variableElimination = object : EliminationAsk() {
+        override fun order(bn: BayesianNetwork?, vars: Collection<RandomVariable>)
+                = super.order(bn, ArrayList(vars).apply {
+            sortWith(comparingInt { -it.domain.size() })
+        })
     }
 
-//    val d1 = bayesInference.ask(
-//            arrayOf<RandomVariable>(ExampleRV.MARY_CALLS_RV),
-//            arrayOf(), bn)
-
-    val d2: CategoricalDistribution = bayesInference.ask(
+    val d2: CategoricalDistribution = variableElimination.ask(
             arrayOf<RandomVariable>(ExampleRV.BURGLARY_RV),
             arrayOf(AssignmentProposition(ExampleRV.MARY_CALLS_RV, true)), bn)
 
