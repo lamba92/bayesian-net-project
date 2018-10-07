9562b26ab30739dce106cc266860fa40ee3ebfad
beppe95 <37592014+beppe95@users.noreply.github.com>
2018-10-08 00:30:32 +0200

    Almost finished.

--- src/main/kotlin/it/unito/probability/bayes/BayesNetsFactory.kt
+++ src/main/kotlin/it/unito/probability/bayes/BayesNetsFactory.kt
@@ -9,6 +9,9 @@ import aima.core.probability.bayes.impl.BayesNet
 import aima.core.probability.bayes.impl.DynamicBayesNet
 import aima.core.probability.bayes.impl.FullCPTNode
 import aima.core.probability.domain.BooleanDomain
+import aima.core.probability.domain.DiscreteDomain
+import aima.core.probability.domain.FiniteDomain
+import aima.core.probability.domain.FiniteIntegerDomain
 import aima.core.probability.util.RandVar
 
 /**
@@ -208,7 +211,111 @@ object BayesNetsFactory{
         return BayesNet(*rootList.toTypedArray())
     }
 
+    fun getSachsNet(): BayesianNetwork{
+        /**
+         * Number of nodes: 11
+         * Number of arcs: 17
+         */
+
+        /**
+         * [RandomVariable]s declaration
+         */
+        val akt = RandVar("AKT", FiniteIntegerDomain())
+        val erk = RandVar("ERK", BooleanDomain())
+        val jnk = RandVar("JNK", BooleanDomain())
+        val mek = RandVar("MEK", BooleanDomain())
+        val p38 = RandVar("P38", BooleanDomain())
+        val pIP2 = RandVar("PIP2", BooleanDomain())
+        val pIP3 = RandVar("PIP3", BooleanDomain())
+        val pKA = RandVar("PKA", BooleanDomain())
+        val pKC = RandVar("PKC", BooleanDomain())
+        val plcg = RandVar("PLCG", BooleanDomain())
+        val raf = RandVar("RAF", BooleanDomain())
+
+        /**
+         * [FullCPTNode]s declaration
+         */
+        val pKC_node = FullCPTNode(pKC, doubleArrayOf(0.5, 0.5))
+        val plcg_node = FullCPTNode(plcg, doubleArrayOf(0.5, 0.5))
+
+        val pKA_node = FullCPTNode(pKA, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02),
+                pKC_node
+        )
+
+        val raf_node = FullCPTNode(raf, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                pKA_node, pKC_node
+        )
+
+        val mek_node =FullCPTNode(mek, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61
+        ), pKA_node, pKC_node, raf_node)
+
+        val erk_node = FullCPTNode(erk, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                mek_node, pKA_node
+        )
+
+        val akt_node = FullCPTNode(erk, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                erk_node, pKA_node
+        )
+
+        val jnk_node = FullCPTNode(jnk, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                pKA_node, pKC_node
+        )
+
+        val p38_node = FullCPTNode(p38, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                pKA_node, pKC_node
+        )
+
+        val pIP3_node = FullCPTNode(pIP3, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02),
+                plcg_node
+        )
+
+        val pIP2_node = FullCPTNode(pIP2, doubleArrayOf(
+                0.02, 0.98,
+                0.98, 0.02,
+                0.98, 0.02,
+                0.02, 0.98),
+                pIP3_node, plcg_node
+        )
+
+        return BayesNet(pKC_node, plcg_node)}
+
     fun getChildNet(): BayesianNetwork {
+        /**
+         * Number of nodes: 20
+         * Number of arcs: 25
+         */
 
         /**
          * [RandomVariable]s declaration
@@ -369,8 +476,11 @@ object BayesNetsFactory{
         return BayesNet(birthAsphyxia_node)
     }
 
-
     fun getAlarmNet(): BayesianNetwork{
+        /**
+         * Number of nodes: 37
+         * Number of arcs: 46
+         */
 
         /**
          * [RandomVariable]s declaration
@@ -622,10 +732,432 @@ object BayesNetsFactory{
                         disconnect_node, minvolset_node)
     }
 
-    fun getAndesNet(): BayesianNetwork{return BayesNet()}
+    fun getHailfinderNet(): BayesianNetwork{
+        /**
+         * Number of nodes: 56
+         * Number of arcs: 66
+         */
 
-    fun getHailfinderNet(): BayesianNetwork{return BayesNet()}
+        /**
+         * [RandomVariable]s declaration
+         */
+        val n0_7muVerMo = RandVar("N0_7MU_VERMO", BooleanDomain())
+        val subjVertMo = RandVar("SUBJ:VERTMO", BooleanDomain())
+        val qGVertMotion = RandVar("QG_VERTMOTION", BooleanDomain())
+        val combVerMo = RandVar("COMB_VERMO", BooleanDomain())
+        val areaMeso_ALS = RandVar("AREAMESO_ALS", BooleanDomain())
+        val satContMoist = RandVar("SATCONTMOIST", BooleanDomain())
+        val raoContMoist = RandVar("RAOCONTMOIST", BooleanDomain())
+        val combMoisture = RandVar("COMB_MOISTURE", BooleanDomain())
 
-    fun getSachsNet(): BayesianNetwork{return BayesNet()}
+        val areaMoDryAir = RandVar("AREAMO_DRYAIR", BooleanDomain())
+        val vISCloudCov = RandVar("VIS_CLOUDCOV", BooleanDomain())
+        val iRCloudCover = RandVar("IR_CLOUDCOV", BooleanDomain())
+        val combClouds = RandVar("COMB_CLOUDS", BooleanDomain())
+        val cldShadeOth = RandVar("CLD_SHADE_OTH", BooleanDomain())
+        val aMInstabMt = RandVar("AN_INSTAB_MT", BooleanDomain())
+        val insInMt = RandVar("INS_IN_MT", BooleanDomain())
+        val wndHodograph = RandVar("WND_HODOGRAPH", BooleanDomain())
 
-}
+        val outflowFrMt = RandVar("OUTFLOW_FRMT", BooleanDomain())
+        val morningBound = RandVar("MORNING_BOUND", BooleanDomain())
+        val boundaries = RandVar("BOUNDARIES", BooleanDomain())
+        val cldShadeConv = RandVar("CLD_SHADE_CONV", BooleanDomain())
+        val compPlFcst = RandVar("COMPL_FCST", BooleanDomain())
+        val capChange = RandVar("CAP_CHANGE", BooleanDomain())
+        val loLevMoistAd = RandVar("LOLEV_MOIST_AD", BooleanDomain())
+        val insChange = RandVar("INS_CHANGE", BooleanDomain())
+
+        val mountainFcst = RandVar("MOUNTAIN_FCST", BooleanDomain())
+        val date = RandVar("DATE", BooleanDomain())
+        val scenario = RandVar("SCENARIO", BooleanDomain())
+        val scenRelAMCIN = RandVar("SCEN_REL_AMCIN", BooleanDomain())
+        val morningCIN = RandVar("MORNING_CIN", BooleanDomain())
+        val aMCINInScen = RandVar("AMCIN_IN_SCEN", BooleanDomain())
+        val capInScen = RandVar("CAP_IN_SCEN", BooleanDomain())
+        val scenRelAMIns = RandVar("SCEN_REL_AMINS", BooleanDomain())
+
+        val lIfr12ZDENSd = RandVar("LIFR23ZDENSD", BooleanDomain())
+        val aMDewptCalPl = RandVar("AMDEWPTCALPL", BooleanDomain())
+        val aMInsWliScen = RandVar("AM_INS_WLISCEN", BooleanDomain())
+        val insSclInScen = RandVar("INS_SCL_INSCEN", BooleanDomain())
+        val scenRel3_4 = RandVar("SCEN_REL3_4", BooleanDomain())
+        val latestCIN = RandVar("LATEST_CIN", BooleanDomain())
+        val lLIW = RandVar("LLIW", BooleanDomain())
+        val curPropConv = RandVar("CUR_PROP_CONV", BooleanDomain())
+
+        val scnRelPlFcst = RandVar("SCN_REL_PLFCST", BooleanDomain())
+        val plainsFcst = RandVar("PLAINS_FCST", BooleanDomain())
+        val n34StarFcst = RandVar("N34_STAR_FCST", BooleanDomain())
+        val r5Fcst = RandVar("R5_FCST", BooleanDomain())
+        val dewpoints = RandVar("DEW_POINTS", BooleanDomain())
+        val lowLLapse = RandVar("LOWL_LAPSE", BooleanDomain())
+        val meanRH = RandVar("MEAN_RH", BooleanDomain())
+        val midLLapse = RandVar("MID_LAPSE", BooleanDomain())
+
+        val mvmtFeatures = RandVar("MVMT_FEATURES", BooleanDomain())
+        val rHRatio = RandVar("RH_RATIO", BooleanDomain())
+        val sfcWndShfDis = RandVar("SFC_WND_SHF_DIS", BooleanDomain())
+        val synForcng = RandVar("SYN_FORCNG", BooleanDomain())
+        val tempDis = RandVar("TEMP_DIS", BooleanDomain())
+        val windAloft = RandVar("WIND_A_LOFT", BooleanDomain())
+        val windFieldMt = RandVar("WIND_FIELD_MT", BooleanDomain())
+        val windFieldPln = RandVar("WIND_FIELD_PIN", BooleanDomain())
+
+        /**
+         * [FullCPTNode]s declaration
+         */
+        val n0_7muVerMo_node = FullCPTNode(n0_7muVerMo, doubleArrayOf(0.5, 0.5))
+        val subjVertMo_node = FullCPTNode(subjVertMo, doubleArrayOf(0.3, 0.7))
+        val qGVertMotion_node = FullCPTNode(qGVertMotion, doubleArrayOf(0.3, 0.7))
+
+        val satContMoist_node = FullCPTNode(satContMoist, doubleArrayOf(0.35, 0.65))
+        val raoContMoist_node = FullCPTNode(raoContMoist, doubleArrayOf(0.35, 0.65))
+        val vISCloudCov_node = FullCPTNode(vISCloudCov, doubleArrayOf(0.6, 0.4))
+
+        val iRCloudCover_node = FullCPTNode(iRCloudCover, doubleArrayOf(0.15, 0.85))
+
+        val aMInstabMt_node = FullCPTNode(aMInstabMt, doubleArrayOf(0.33, 0.67))
+        val wndHodograph_node = FullCPTNode(wndHodograph, doubleArrayOf(0.5, 0.5))
+        val morningBound_node = FullCPTNode(morningBound, doubleArrayOf(0.5, 0.5))
+
+        val loLevMoistAd_node = FullCPTNode(loLevMoistAd, doubleArrayOf(0.4, 0.6))
+
+        val date_node = FullCPTNode(date, doubleArrayOf(0.5, 0.5))
+        val morningCIN_node = FullCPTNode(morningCIN, doubleArrayOf(0.72, 0.28))
+        val lIfr12ZDENSd_node = FullCPTNode(lIfr12ZDENSd, doubleArrayOf(0.62, 0.38))
+
+        val aMDewptCalPl_node = FullCPTNode(aMDewptCalPl, doubleArrayOf(0.55, 0.45))
+        val latestCIN_node = FullCPTNode(latestCIN, doubleArrayOf(0.8, 0.2))
+        val lLIW_node = FullCPTNode(lLIW, doubleArrayOf(0.44, 0.56))
+
+        val combVerMo_node = FullCPTNode(combVerMo, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61),
+                n0_7muVerMo_node, subjVertMo_node, qGVertMotion_node
+        )
+
+        val areaMeso_ALS_node = FullCPTNode(areaMeso_ALS, doubleArrayOf(
+                0.95, 0.05,
+                0.05, 0.95),
+                combVerMo_node
+        )
+
+        val combMoisture_node = FullCPTNode(combMoisture, doubleArrayOf(
+                0.9, 0.1,
+                0.55, 0.45,
+                0.45, 0.55,
+                0.25, 0.75),
+                satContMoist_node, raoContMoist_node
+        )
+
+        val areaMoDryAir_node = FullCPTNode(areaMoDryAir, doubleArrayOf(
+                0.99, 0.01,
+                0.7, 0.3,
+                0.4, 0.6,
+                0.02, 0.98),
+                areaMeso_ALS_node, combMoisture_node
+        )
+
+        val combClouds_node = FullCPTNode(combClouds, doubleArrayOf(
+                0.95, 0.05,
+                0.58, 0.42,
+                0.3, 0.7,
+                0.05, 0.95),
+                vISCloudCov_node, iRCloudCover_node
+        )
+
+        val cldShadeOth_node = FullCPTNode(cldShadeOth, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61),
+                areaMoDryAir_node, areaMeso_ALS_node, combClouds_node
+        )
+
+
+        val insInMt_node = FullCPTNode(insInMt, doubleArrayOf(
+                0.99, 0.01,
+                0.7, 0.3,
+                0.4, 0.6,
+                0.02, 0.98),
+                cldShadeOth_node, aMInstabMt_node
+        )
+
+        val outflowFrMt_node = FullCPTNode(outflowFrMt, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                insInMt_node, wndHodograph_node
+        )
+
+        val boundaries_node = FullCPTNode(boundaries, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61),
+                outflowFrMt_node, wndHodograph_node, morningBound_node
+        )
+
+        val cldShadeConv_node = FullCPTNode(cldShadeConv, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                insInMt_node, wndHodograph_node
+        )
+
+        val compPlFcst_node =  FullCPTNode(compPlFcst, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61,
+                0.55, 0.45,
+                0.17, 0.83,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.44, 0.56,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.19, 0.81
+        ), boundaries_node, cldShadeConv_node, areaMeso_ALS_node, cldShadeOth_node)
+
+        val capChange_node = FullCPTNode(capChange, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                compPlFcst_node
+        )
+
+        val insChange_node = FullCPTNode(insChange, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                loLevMoistAd_node, compPlFcst_node
+        )
+
+        val mountainFcst_node = FullCPTNode(mountainFcst, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                insInMt_node
+        )
+
+        val scenario_node = FullCPTNode(scenario, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                date_node
+        )
+
+        val scenRelAMCIN_node = FullCPTNode(scenRelAMCIN, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val aMCINInScen_node = FullCPTNode(aMCINInScen, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                scenRelAMCIN_node, morningCIN_node
+        )
+
+        val capInScen_node = FullCPTNode(insChange, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                aMCINInScen_node, capChange_node
+        )
+
+        val scenRelAMIns_node = FullCPTNode(scenRelAMIns, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val aMInsWliScen_node = FullCPTNode(aMInsWliScen, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61),
+                scenRelAMIns_node, lIfr12ZDENSd_node, aMDewptCalPl_node
+        )
+
+        val insSclInScen_node = FullCPTNode(insSclInScen, doubleArrayOf(
+                0.9, 0.1,
+                0.08, 0.92),
+                aMInsWliScen_node
+        )
+
+        val scenRel3_4_node = FullCPTNode(scenRel3_4, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val curPropConv_node = FullCPTNode(curPropConv, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                latestCIN_node, lLIW_node
+        )
+
+        val scnRelPlFcst_node = FullCPTNode(scnRelPlFcst, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val plainsFcst_node =  FullCPTNode(plainsFcst, doubleArrayOf(
+                0.97, 0.03,
+                0.31, 0.69,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.3, 0.7,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.39, 0.61,
+                0.55, 0.45,
+                0.17, 0.83,
+                0.02, 0.98,
+                0.98, 0.02,
+                0.44, 0.56,
+                0.25, 0.75,
+                0.98, 0.02,
+                0.19, 0.81
+        ), curPropConv_node, insSclInScen_node, capInScen_node, scnRelPlFcst_node)
+
+        val n34StarFcst_node = FullCPTNode(n34StarFcst, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                scenRel3_4_node, plainsFcst_node
+        )
+
+        val r5Fcst_node = FullCPTNode(r5Fcst, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5,
+                0.35, 0.65,
+                0.08, 0.92),
+                mountainFcst_node, n34StarFcst_node
+        )
+
+        val dewpoints_node = FullCPTNode(dewpoints, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val lowLLapse_node = FullCPTNode(lowLLapse, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val meanRH_node = FullCPTNode(meanRH, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val midLLapse_node = FullCPTNode(midLLapse, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val mvmtFeatures_node = FullCPTNode(mvmtFeatures, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val rHRatio_node = FullCPTNode(rHRatio, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val sfcWndShfDis_node = FullCPTNode(sfcWndShfDis, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val synForcng_node = FullCPTNode(synForcng, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val tempDis_node = FullCPTNode(tempDis, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val windAloft_node = FullCPTNode(windAloft, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val windFieldMt_node = FullCPTNode(windFieldMt, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        val windFieldPln_node = FullCPTNode(windFieldPln, doubleArrayOf(
+                0.9, 0.1,
+                0.5, 0.5),
+                scenario_node
+        )
+
+        return BayesNet(n0_7muVerMo_node, subjVertMo_node, qGVertMotion_node,
+                satContMoist_node, raoContMoist_node, vISCloudCov_node,
+                iRCloudCover_node, aMInstabMt_node, wndHodograph_node,
+                morningBound_node, loLevMoistAd_node, date_node, morningCIN_node,
+                lIfr12ZDENSd_node, aMDewptCalPl_node, latestCIN_node, lLIW_node)
+    }}
+
+    fun getPathfinderNet(): BayesianNetwork{
+        /**
+         * Number of nodes: 135
+         * Number of arcs: 200
+         */
+
+        /**
+         * [RandomVariable]s declaration
+         */
+        return BayesNet()
+    }
+
+
+
